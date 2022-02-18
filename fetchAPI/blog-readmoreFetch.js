96970ab0478eeb95c2a8b693a17ee0980b2c966b
Eric Maniraguha <ericmaniraguha@gmail.com>
2022-02-18 18:58:01 +0200

    comment working semi

--- fetchAPI/blog-readmoreFetch.js
+++ fetchAPI/blog-readmoreFetch.js
@@ -28,7 +28,7 @@ readMore();
 const input1 = formPost.elements[0];
 const input2 = formPost.elements[1];
 const postBtn = document.querySelector('.comment-submit');
-const com = document.querySelector('#list-comment');
+const com = document.querySelector('.read-comment');
 
 const addComment = async () =>{
     var obj = {
@@ -42,15 +42,19 @@ const addComment = async () =>{
          headers:{
             'Content-Type':'application/json'
          }
+        
     })
-    if(comment.status === 201){
+     
+    if(comment.status === 201 ){
     const results = await comment.json();
-    console.log(results);
+  
 
-    const commentAdded = results.data;
+    const commentAdded = results;
+  
     console.log(commentAdded);
     for (let i = 0; i < commentAdded.length; i++)
     {
+      console.log(commentAdded);
 
 
       console.log("commentAdded[i].name");
@@ -78,16 +82,18 @@ const addComment = async () =>{
 
 formPost.addEventListener('submit', (e) => {
     e.preventDefault();
+
+     validationFun();  
+      addComment();
+      getComments();
+      location.reload();
   
-    addComment();
-    // saveComment();
-    // location.reload();
   })
 
 
 function validationFun() { 
   if (input1.value == "" && input2.value == "") {
-
+    alert('Your field must be filled')
     input1.style.border = "solid 1px red";
     input2.style.border = "solid 1px red";
     input1_invalid.style.display = "block";
@@ -96,35 +102,40 @@ function validationFun() {
 
 }
 
-// function addComment() {
- 
-//   Readmessage.comment.push(obj);
-//   localStorage.setItem("article", JSON.stringify(getblog));
-//   formPost.reset();
-// }
-// console.log(Readmessage.comment);
-// const com = document.querySelector('#list-comment');
+//get comments 
 
-// function saveComment() {
- 
-//   for (let i = 0; i < Readmessage.comment.length; i++)
-//   {
-//     const el3 = document.createElement('h3');
-//     const el2 = document.createElement('h2');
-//     const span = document.createElement('span');
-    
-//     el3.style.fontSize = "12px";
-//     span.style.fontSize = "10px"
+const getComments = () => {
+  const myComments = document.querySelector(".comment-area");
+  let response = fetch(
+    `https://my-brand-api-1.herokuapp.com/api/v1/comments/${id}`,
+    {
+      method: "GET",
+    }
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((comments) => {
+      console.log(comments);
+      const content = comments.data
+      console.log(content)
+      content.map((comment) => {
+        let html1 = `
 
-//     el2.style.color = "blue";
-//     el2.style.fontSize = "12px";
+            <div class="comment-area">
+            <img
+              class="comment-avatar"src="https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight&accessoriesType=Blank&hairColor=BrownDark&facialHairType=Blank&clotheType=BlazerShirt&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light"
+            />
+            <div>
+              <h3 class="comment-author">${comment.name} <span id="comment_date">${comment.create_at}</span></h3> <br> <br>
+              <p class="comment-body">${comment.comment}</p> 
+            </div>
+          </div>
 
-//     el3.textContent = Readmessage.comment[i].Name;
-//     el2.textContent = Readmessage.comment[i].Comment;
-//     span.textContent = Readmessage.comment[i].Time;
-//     com.append(el3, el2, span);
-//   }
-
-// }
- 
-// saveComment();
+                   `;
+        myComments.innerHTML += html1;
+      });
+    })
+    .catch((error) => console.log(error));
+};
+getComments();
